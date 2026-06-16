13f0215ca4a9c88a075f4ab6b7c0a7c30d3562a2
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2026-06-16 10:56:00 -0300

    fix: refactor exercicio-03

--- lista-01/exercicio-03.c
+++ lista-01/exercicio-03.c
@@ -1,11 +1,27 @@
+// 3. Fazer um programa que recebe um símbolo de operação do usuário (+, -, / ou *) e dois números reais.
+// O programa deve retornar o resultado da operação recebida sobre estes dois números.
+ 
 #include <stdio.h>
-#include <stdlib.h>
 
-// 3. Fazer um programa que recebe um símbolo de operação do usuário (+, -, / ou *) e dois 
-// números reais. O programa deve retornar o resultado da operação recebida sobre estes 
-// dois números. 
+float retornarResultado(int a, int b, char c);
 
-int main()
-{
+int main() {
+    int a, b;
+    char c;
+ 
+    printf("Digite dois numeros: ");
+    scanf("%d %d", &a, &b);
 
+    printf("Digite a operacao (+, -, *, /): ");
+    scanf(" %c", &c);
+
+    printf("Resultado: %.2f", retornarResultado(a, b, c));
+}
+
+float retornarResultado(int a, int b, char c) {
+    if (c == '+') return a + b;
+    if (c == '-') return a - b;
+    if (c == '*') return a * b;
+    if (c == '/') return a / b;
+    else printf("Operacao invalida!");
 }
