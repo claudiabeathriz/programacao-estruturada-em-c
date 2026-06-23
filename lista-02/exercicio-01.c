d6e3a469ff4abdf93ce7bf28b14e172e9ea20652
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2026-06-23 10:56:00 -0300

    feat: add exercicio-01 lista-02

--- lista-02/exercicio-01.c
+++ lista-02/exercicio-01.c
@@ -3,3 +3,37 @@
 // O programa deve imprimir a mensagem “NUMERO PRIMO” 
 // caso o número seja primo, e “NUMERO NAO PRIMO” caso contrário.
 
+#include <stdio.h>
+#include <stdbool.h>
+
+void isPrime(int num);
+
+int main() {
+    int num;
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    isPrime(num);
+}
+
+void isPrime(int num) {
+    int i = 1;
+    bool isPrime = true;
+
+    if (num <= 1) {
+        isPrime = false; // numeros menores ou iguais a 1 nao sao primos
+    } else {
+        for (i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = false; // encontrou um divisor, nao primo
+                break;
+            }
+        }
+    }
+
+    if (isPrime) {
+        printf("NUMERO PRIMO\n");
+    } else {
+        printf("NUMERO NAO PRIMO\n");
+    }
+}
